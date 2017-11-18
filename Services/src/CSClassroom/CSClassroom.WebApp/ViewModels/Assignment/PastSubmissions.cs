5915350807a00d8e25a78ba883b9ff0094a502f7
Andrew Smith <smithandr@hotmail.com>
2017-11-18 12:19:51 -0800

    Fix past submission links for admins viewing student submissions

---
+++ Services/src/CSClassroom/CSClassroom.WebApp/ViewModels/Assignment/PastSubmissions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSC.CSClassroom.WebApp.ViewModels.Assignment
+{
+	/// <summary>
+	/// The list of past submissions for a question by a given user.
+	/// </summary>
+	public class PastSubmissions
+	{
+		/// <summary>
+		/// The user ID.
+		/// </summary>
+		public int UserId { get; }
+
+		/// <summary>
+		/// The times of the user's previous submissions.
+		/// </summary>
+		public IList<DateTime> SubmissionTimes { get; }
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public PastSubmissions(int userId, IList<DateTime> submissionTimes)
+		{
+			UserId = userId;
+			SubmissionTimes = submissionTimes;
+		}
+	}
+}
