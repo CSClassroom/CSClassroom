51acff71e11e7d3767c57d6b0562104fa3d7470d
dave-br <dave-br@users.noreply.github.com>
2020-05-10 09:01:29 -0700

    start support for selecting individual students for DL

--- Services/src/CSClassroom/CSClassroom.WebApp/ViewModels/Submission/DownloadSubmissionViewModel.cs
+++ Services/src/CSClassroom/CSClassroom.WebApp/ViewModels/Submission/DownloadSubmissionViewModel.cs
@@ -7,23 +7,69 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CSC.CSClassroom.WebApp.ViewModels.Submission
 {
-    public class DownloadSubmissionViewModel
-    {
+	/// <summary>
+	/// The category of submission components to download
+	/// </summary>
+	public enum DownloadFormat
+	{
+		[Display(Name = "Flat file list")]
+		Flat,
+
+		[Display(Name = "Eclipse projects")]
+		Eclipse,
+
+		[Display(Name = "All")]
+		All,
+	}
+
+	/// <summary>
+	/// Info on the "current section", which is what the user had clicked on before
+	/// choosing to Download.  This is used to populate the default section option
+	/// from the Download page.
+	/// </summary>
+	public class SectionInfo
+	{
+		public string Name { get; set; }
+		public int Index { get; set; }
+	}
+
+    /// <summary>
+    ///  Info for a single student to download
+    /// </summary>
+    public class StudentToDownload
+	{
+		public string StudentName { get; set; }
+		public int StudentID { get; set; }
+	}
+
+	/// <summary>
+	/// Info on each section to download, including the students within
+	/// that section to download.
+	/// </summary>
+	public class SectionStudentsToDownload
+	{
 		/// <summary>
-		/// The category of submission components to download
+		///  Name of section selected for download
 		/// </summary>
-		public enum DownloadFormat
-		{
-			[Display(Name = "Flat file list")]
-			Flat,
+		public SelectListItem SectionName { get; set; }
 
-			[Display(Name = "Eclipse projects")]
-			Eclipse,
+		/// <summary>
+		/// If true, download ALL students from this section, and ignore SelectedStudents
+		/// </summary>
+		public Boolean AllStudents { get; set; }
 
-			[Display(Name = "All")]
-			All,
-		}
+		/// <summary>
+		///  Students selected by user to download
+		/// </summary>
+		public List<StudentToDownload> SelectedStudents { get; set; }
+	}
 
+	/// <summary>
+	/// The master view model for the download submission page, used for downloading
+	/// Eclipse projects and flat file lists from student project repos
+	/// </summary>
+	public class DownloadSubmissionViewModel
+    {
 		[Display
 		(
 			Name = "Components to download",
@@ -36,13 +82,8 @@ namespace CSC.CSClassroom.WebApp.ViewModels.Submission
 			Name = "Sections",
 			Description = "Select the sections to download."
 		)]
-		public List<SelectListItem> SectionNames { get; set; }
+		public List<SectionStudentsToDownload> SectionStudents { get; set; }
 
-		public class SectionInfo
-		{
-			public string Name { get; set; }
-			public int Index { get; set; }
-		}
         public SectionInfo CurrentSection { get; set; }
 	}
 }
